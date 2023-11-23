f3adb048245b9479f784feebfe537bdc520f6e43
Ezequiel <ki3lmigu3l@gmail.com>
2023-11-23 12:48:12 -0300

    Método GET para listar os clientes

--- api/src/main/java/com/github/ki3lmigu3l/api/controller/ClienteController.java
+++ api/src/main/java/com/github/ki3lmigu3l/api/controller/ClienteController.java
@@ -1,6 +1,9 @@
 package com.github.ki3lmigu3l.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +22,10 @@ public class ClienteController {
     @PostMapping
     public Cliente cadastrar(@RequestBody Cliente cliente) {
          return clienteRepository.save(cliente);
+    }
+
+    @GetMapping
+    public List<Cliente> listarClientes () {
+        return clienteRepository.findAll();
     }
 }
