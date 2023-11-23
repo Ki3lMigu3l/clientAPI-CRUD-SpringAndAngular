32619a15e7bd2fd7966cbed7703f4f05daca7fc9
Ezequiel <ki3lmigu3l@gmail.com>
2023-11-23 12:44:50 -0300

    Método POST para cadastrar os clientes

--- api/src/main/java/com/github/ki3lmigu3l/api/controller/ClienteController.java
+++ api/src/main/java/com/github/ki3lmigu3l/api/controller/ClienteController.java
@@ -1,11 +1,23 @@
 package com.github.ki3lmigu3l.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.github.ki3lmigu3l.api.model.Cliente;
+import com.github.ki3lmigu3l.api.repository.ClienteRepository;
 
 @RestController
 @RequestMapping("cliente")
 public class ClienteController {
     
-    
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @PostMapping
+    public Cliente cadastrar(@RequestBody Cliente cliente) {
+         return clienteRepository.save(cliente);
+    }
 }
