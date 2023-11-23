ed212f64ad09333f19de9a90fa74e69774193bb9
Ezequiel <ki3lmigu3l@gmail.com>
2023-11-23 12:39:05 -0300

    Mapeando a entidade de Cliente para a nossa base de dados

--- api/src/main/java/com/github/ki3lmigu3l/api/model/Cliente.java
+++ api/src/main/java/com/github/ki3lmigu3l/api/model/Cliente.java
@@ -1,5 +1,24 @@
 package com.github.ki3lmigu3l.api.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "clientes")
+@Getter
+@Setter
 public class Cliente {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private int idade;
+    private String cidade;
+
 }
